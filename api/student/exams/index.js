e0a7db826eb0db227fd612c263c1631345146524
Govind Mishra <shreegovind339@gmail.com>
2026-09-09 01:08:15 +0530

    fix(api): wrap student exams handler in outer try-catch

--- api/student/exams/index.js
+++ api/student/exams/index.js
@@ -3,18 +3,18 @@ import { listEnrolledExamsForStudent } from '../_lib/students.js';
 import { sendJson } from '../_lib/http.js';
 
 export default async function handler(req, res) {
-  const auth = requireStudent(req);
-  if (auth.error) {
-    sendJson(res, auth.status, { error: auth.error });
-    return;
-  }
-
-  if (req.method !== 'GET') {
-    sendJson(res, 405, { error: 'Method not allowed' });
-    return;
-  }
-
   try {
+    const auth = requireStudent(req);
+    if (auth.error) {
+      sendJson(res, auth.status, { error: auth.error });
+      return;
+    }
+
+    if (req.method !== 'GET') {
+      sendJson(res, 405, { error: 'Method not allowed' });
+      return;
+    }
+
     const exams = await listEnrolledExamsForStudent(auth.studentId);
     sendJson(res, 200, { exams });
   } catch (err) {
